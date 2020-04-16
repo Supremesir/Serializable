cc6d50acb735c9cbbc7a39e74c161d672b58eac5
Haofan Fang <haofanfang@gmail.com>
2020-04-16 18:12:58 +0800

    update Serializable save and load

--- app/src/main/java/com/supremesir/serializable/MainActivity.java
+++ app/src/main/java/com/supremesir/serializable/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.supremesir.serializable.databinding.ActivityMainBinding;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,12 +38,36 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     ObjectOutputStream objectOutputStream = new ObjectOutputStream(openFileOutput(getResources().getString(R.string.FILE_NAME), MODE_PRIVATE));
                     objectOutputStream.writeObject(student);
+                    objectOutputStream.flush();
+                    objectOutputStream.close();
+                    // 保存数据完成后，清空editText
+                    binding.editTextName.getText().clear();
+                    binding.editTextAge.getText().clear();
+                    binding.editTextMath.getText().clear();
+                    binding.editTextEnglish.getText().clear();
+                    binding.editTextChinese.getText().clear();
+                    binding.textView.setText("-");
+                    Toast.makeText(MainActivity.this, "Data Saved!", Toast.LENGTH_SHORT).show();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            }
+        });
 
-
-                
+        binding.buttonLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    ObjectInputStream objectInputStream = new ObjectInputStream(openFileInput(getResources().getString(R.string.FILE_NAME)));
+                    Student student = (Student) objectInputStream.readObject();
+                    binding.editTextName.setText(student.getName());
+                    binding.editTextAge.setText(student.getAge());
+                    binding.editTextMath.setText(student.getScore().getMathScore());
+                    binding.editTextEnglish.setText(student.getScore().getEnglishScore());
+                    binding.editTextChinese.setText(student.getScore().getChineseScore());
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
